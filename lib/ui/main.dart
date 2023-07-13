af6c5459f91003611f1ebba83e1b669936ea53a4
Wsy <wsy@@gmail.com>
2023-07-13 12:04:18 +0800

    首页开发

--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -34,12 +34,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // 当前页面索引
   int _currentIndex = 0;
 
   // 页面列表
   List<Widget> pages = [
-    HomePage(),
-    DataPage(),
+    const HomePage(),
+    const DataPage(),
     const IOPage(),
     const MinePage()
   ];
@@ -99,33 +100,38 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       // 底部导航栏
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _currentIndex,
-        items: bottomNavigationBarItem,
-        selectedFontSize: 12,
-        unselectedFontSize: 10,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        // 设置选中时的文字颜色
-        selectedItemColor: colorPrimary,
-        // 设置未选中时的文字颜色
-        unselectedItemColor: colorGrey515151,
-        onTap: (index) {
-          // 点击时改变_currentIndex，如何触发刷新页面
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-      ),
+      bottomNavigationBar: _bottomNavigationBar(),
       body: pages[_currentIndex],
+    );
+  }
+
+  /// 底部导航栏
+  BottomNavigationBar _bottomNavigationBar() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: _currentIndex,
+      items: bottomNavigationBarItem,
+      selectedFontSize: 12,
+      unselectedFontSize: 10,
+      showSelectedLabels: true,
+      showUnselectedLabels: true,
+      // 设置选中时的文字颜色
+      selectedItemColor: colorPrimary,
+      // 设置未选中时的文字颜色
+      unselectedItemColor: colorGrey515151,
+      onTap: (index) {
+        // 点击时改变_currentIndex，如何触发刷新页面
+        setState(() {
+          _currentIndex = index;
+        });
+      },
     );
   }
 }
 
 /// 首页
 class HomePage extends StatefulWidget {
-  HomePage({super.key});
+  const HomePage({super.key});
 
   @override
   State<StatefulWidget> createState() {
@@ -217,132 +223,7 @@ class _HomePageState extends State<HomePage> {
               // 本月预算
               _buildBudgetView(),
               // 待办事项
-              Container(
-                width: MediaQuery.of(context).size.width,
-                // 宽度铺满
-                margin: const EdgeInsets.only(left: 10, top: 10, right: 10),
-                padding: const EdgeInsets.only(top: 10, bottom: 10),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(6)),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        const Text(
-                          "待办事项",
-                          style: TextStyle(
-                              fontSize: 14, fontWeight: FontWeight.bold),
-                        ),
-                        Expanded(
-                            child: SizedBox(
-                          height: 20,
-                          child: IconButton(
-                            splashColor: Colors.transparent,
-                            highlightColor: Colors.transparent,
-                            alignment: Alignment.centerRight,
-                            padding: const EdgeInsets.all(0),
-                            iconSize: 14,
-                            icon: const Icon(
-                              Icons.arrow_forward_ios_outlined,
-                              color: colorGrey707070,
-                            ),
-                            onPressed: () {
-                              print("点击了待办事项");
-                            },
-                          ),
-                        )),
-                        const SizedBox(
-                          width: 10,
-                        )
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(left: 10, right: 10),
-                      child: const Divider(
-                        height: 1,
-                        color: colorDivider,
-                      ),
-                    ),
-                    Column(
-                      children: _todoList.map((e) {
-                        return Column(
-                          children: [
-                            Container(
-                              margin: const EdgeInsets.only(top: 10),
-                              child: Row(
-                                children: [
-                                  Container(
-                                    width: 9,
-                                    height: 18,
-                                    decoration: const BoxDecoration(
-                                        color: colorPrimary,
-                                        borderRadius: BorderRadius.only(
-                                            topRight: Radius.circular(9),
-                                            bottomRight: Radius.circular(9))),
-                                  ),
-                                  const SizedBox(
-                                    width: 10,
-                                  ),
-                                  Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        e.title.toString(),
-                                        style: const TextStyle(
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      const SizedBox(
-                                        height: 6,
-                                      ),
-                                      Text(
-                                        e.description.toString(),
-                                        style: const TextStyle(fontSize: 12),
-                                      )
-                                    ],
-                                  ),
-                                  Expanded(
-                                      child: Container(
-                                    alignment: Alignment.centerRight,
-                                    child: Text(
-                                      e.date.toString(),
-                                      style: const TextStyle(fontSize: 12),
-                                    ),
-                                  )),
-                                  const SizedBox(
-                                    width: 10,
-                                  ),
-                                ],
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            if (_todoList.indexOf(e) != _todoList.length - 1)
-                              Container(
-                                padding:
-                                    const EdgeInsets.only(left: 10, right: 10),
-                                child: const Divider(
-                                  height: 1,
-                                  color: colorDivider,
-                                ),
-                              )
-                          ],
-                        );
-                      }).toList(),
-                    )
-                  ],
-                ),
-              ),
+              _buildTodoView(),
               const SizedBox(
                 height: 10,
               ),
@@ -353,64 +234,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  /// 预算设置弹窗
-  _showModalBottomSheet(BuildContext context) {
-    List<int> list = [500, 1000, 1500, 2000, 2500, 500, 1000, 1500, 2000, 2500];
-    showModalBottomSheet(
-        context: context,
-        builder: (BuildContext context) {
-          return Container(
-            decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10))),
-            padding: const EdgeInsets.only(top: 10),
-            width: MediaQuery.of(context).size.width,
-            height: 300,
-            child: Column(
-              children: [
-                const Text(
-                  "预算设置",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Divider(
-                  height: 1,
-                  color: colorDivider,
-                ),
-                Expanded(
-                    child: ListView(
-                        children: list.map((e) {
-                  return TextButton(
-                    style: ButtonStyle(
-                      padding:
-                          MaterialStateProperty.all(const EdgeInsets.all(8)),
-                      // 取消点击效果
-                      overlayColor:
-                          MaterialStateProperty.all(Colors.transparent),
-                    ),
-                    onPressed: () {
-                      // 计算当月预算
-                      setState(() {
-                        percent = consume / e.toDouble();
-                      });
-                      // 底部弹窗消失
-                      Navigator.pop(context);
-                    },
-                    child: Text(e.toString(),
-                        style:
-                            const TextStyle(fontSize: 14, color: Colors.black)),
-                  );
-                }).toList()))
-              ],
-            ),
-          );
-        });
-  }
-
+  /// 轮播图
   Widget _buildBannerView() {
     return Container(
       clipBehavior: Clip.antiAlias,
@@ -442,6 +266,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  /// 本月预算
   Widget _buildBudgetView() {
     return Container(
       // 宽度铺满
@@ -495,20 +320,8 @@ class _HomePageState extends State<HomePage> {
             children: [
               Container(
                 margin: const EdgeInsets.only(left: 10),
-                child: CircularPercentIndicator(
-                  radius: 56,
-                  lineWidth: 14,
-                  percent: percent,
-                  animation: true,
-                  animationDuration: 500,
-                  center: Text(
-                    "剩余：${(percent * 100).truncate()}%",
-                    style: const TextStyle(
-                        fontSize: 12, fontWeight: FontWeight.bold),
-                  ),
-                  progressColor: colorPrimary,
-                  backgroundColor: colorDivider,
-                ),
+                // 预算进度条
+                child: _buildCircleProgress(),
               ),
               const SizedBox(
                 width: 20,
@@ -576,6 +389,213 @@ class _HomePageState extends State<HomePage> {
           ))
         ],
       ),
+    );
+  }
+
+  /// 本月预算进度条
+  Widget _buildCircleProgress() {
+    return CircularPercentIndicator(
+      radius: 56,
+      lineWidth: 14,
+      percent: percent,
+      animation: true,
+      animationDuration: 500,
+      center: Text(
+        "剩余：${(percent * 100).truncate()}%",
+        style: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+      ),
+      progressColor: colorPrimary,
+      backgroundColor: colorDivider,
+    );
+  }
+
+  /// 预算设置弹窗
+  _showModalBottomSheet(BuildContext context) {
+    List<int> list = [500, 1000, 1500, 2000, 2500, 500, 1000, 1500, 2000, 2500];
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10))),
+            padding: const EdgeInsets.only(top: 10),
+            width: MediaQuery.of(context).size.width,
+            height: 300,
+            child: Column(
+              children: [
+                const Text(
+                  "预算设置",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Divider(
+                  height: 1,
+                  color: colorDivider,
+                ),
+                Expanded(
+                    child: ListView(
+                        children: list.map((e) {
+                  return _buildBudgetItemView(e);
+                }).toList()))
+              ],
+            ),
+          );
+        });
+  }
+
+  /// 本月预算item view
+  Widget _buildBudgetItemView(int e) {
+    return TextButton(
+      style: ButtonStyle(
+        padding: MaterialStateProperty.all(const EdgeInsets.all(8)),
+        // 取消点击效果
+        overlayColor: MaterialStateProperty.all(Colors.transparent),
+      ),
+      onPressed: () {
+        // 计算当月预算
+        setState(() {
+          percent = consume / e.toDouble();
+        });
+        // 底部弹窗消失
+        Navigator.pop(context);
+      },
+      child: Text(e.toString(),
+          style: const TextStyle(fontSize: 14, color: Colors.black)),
+    );
+  }
+
+  /// 待办事项
+  Widget _buildTodoView() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      // 宽度铺满
+      margin: const EdgeInsets.only(left: 10, top: 10, right: 10),
+      padding: const EdgeInsets.only(top: 10, bottom: 10),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(6)),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(
+                width: 10,
+              ),
+              const Text(
+                "待办事项",
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+              ),
+              Expanded(
+                  child: SizedBox(
+                height: 20,
+                child: IconButton(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  alignment: Alignment.centerRight,
+                  padding: const EdgeInsets.all(0),
+                  iconSize: 14,
+                  icon: const Icon(
+                    Icons.arrow_forward_ios_outlined,
+                    color: colorGrey707070,
+                  ),
+                  onPressed: () {
+                    print("点击了待办事项");
+                  },
+                ),
+              )),
+              const SizedBox(
+                width: 10,
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: const Divider(
+              height: 1,
+              color: colorDivider,
+            ),
+          ),
+          Column(
+            children: _todoList.map((e) {
+              return _buildTodoItemView(e);
+            }).toList(),
+          )
+        ],
+      ),
+    );
+  }
+
+  /// 待办事项ListView item
+  Widget _buildTodoItemView(TodoBean e) {
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 10),
+          child: Row(
+            children: [
+              Container(
+                width: 9,
+                height: 18,
+                decoration: const BoxDecoration(
+                    color: colorPrimary,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(9),
+                        bottomRight: Radius.circular(9))),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    e.title.toString(),
+                    style: const TextStyle(
+                        fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    e.description.toString(),
+                    style: const TextStyle(fontSize: 12),
+                  )
+                ],
+              ),
+              Expanded(
+                  child: Container(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  e.date.toString(),
+                  style: const TextStyle(fontSize: 12),
+                ),
+              )),
+              const SizedBox(
+                width: 10,
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        if (_todoList.indexOf(e) != _todoList.length - 1)
+          Container(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: const Divider(
+              height: 1,
+              color: colorDivider,
+            ),
+          )
+      ],
     );
   }
 }
